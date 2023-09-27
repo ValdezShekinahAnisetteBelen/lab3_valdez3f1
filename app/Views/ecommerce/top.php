e85a16d15c8161c91954ccedb29d56cbdb66c318
Valdez Shekinah Anisette Belen <shekinahvaldez2003@gmail.com>
2023-09-27 20:59:52 +0800

    new

---
+++ app/Views/ecommerce/top.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<!-- Mirrored from herba-preview-com.3dcartstores.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 26 Sep 2023 13:57:49 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>Welcome to Sample Store  - Sample Slogan</title>
+<link rel="canonical" href=" <?= base_url() ?> index.html" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2, user-scalable=yes">
+
+<link rel="alternate" type="application/rss+xml" title="Featured Items (RSS 2.0)" href=" <?= base_url() ?> rss4ab8.asp?type=home" />
+<link rel="alternate" type="application/rss+xml" title="Products On Sale (RSS 2.0)" href=" <?= base_url() ?> rss4291.asp?type=onsale" />
+<link rel="alternate" type="application/rss+xml" title="New Releases (RSS 2.0)" href=" <?= base_url() ?> rssb42c.asp?type=newreleases" />
+<link rel="alternate" type="application/rss+xml" title="Best Sellers (RSS 2.0)" href=" <?= base_url() ?> rssd2f6.asp?type=bestsellers" />
+<link rel="alternate" type="application/rss+xml" title="Latest Blog Posts (RSS 2.0)" href=" <?= base_url() ?> rss6f62.asp?type=blog" />
+
+
+<link rel="preconnect" href=" <?= base_url() ?> https://fonts.gstatic.com/">
+<link href=" <?= base_url() ?> https://fonts.googleapis.com/css2?family=Alice&amp;family=Roboto:wght@400;500&amp;display=swap" rel="stylesheet">
+<link rel="stylesheet" href=" <?= base_url() ?> assets/templates/common-core/lib/bootstrap/css/bootstrap1043.css?vcart=13.2.11" type="text/css" />
+<link rel="StyleSheet" href=" <?= base_url() ?> assets/templates/common-core/css/core1043.css?vcart=13.2.11" type="text/css" />
+<link rel="stylesheet" href=" <?= base_url() ?> assets/templates/common-core/lib/flexslider/flexslider1043.css?vcart=13.2.11" type="text/css" media="screen" />
+<link href=" <?= base_url() ?> assets/templates/common-core/lib/contentbuilder/minimalist-blocks/content.min1043.css?vcart=13.2.11" rel="stylesheet" type="text/css" />
+<link rel="StyleSheet" href=" <?= base_url() ?> assets/templates/herba-core/css/default1043.css?vcart=13.2.11" type="text/css" />
+
+<script type="text/javascript">var _3d_cart = {"oid":0,"itemcount":0,"itemsum":0,"subtotal":0,"currency":"USD"};</script>
+</head>
